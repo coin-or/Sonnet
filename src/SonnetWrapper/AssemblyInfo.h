f9144f7764e1185f8a53ca339c94e50e31236cd5
Jan-Willem Goossens <jhmgoossens@hotmail.com>
2020-09-05 14:50:31 +0200

    Include Sonnet-1.3 bin files

--- src/SonnetWrapper/AssemblyInfo.h
+++ src/SonnetWrapper/AssemblyInfo.h
@@ -4,8 +4,8 @@
 
 #pragma once
 
-#define VER_FILEVERSION			1,2,0,0
-#define VER_FILEVERSION_STR		"1.2.0.0"
+#define VER_FILEVERSION			1,3,0,0
+#define VER_FILEVERSION_STR		"1.3.0.0"
 
 // About Production & Assembly version:
 // Did the interface change? 
@@ -13,25 +13,25 @@
 //         -> No: Then change the version number
 //         -> Yes: Keep same version. 
 // -> No: Keep same version.
-#define VER_PRODUCTVERSION		1,2,0,0
-#define VER_PRODUCTVERSION_STR	"1.2.0.0"
-#define VER_ASSEMBLYVERSION_STR	"1.2.0.0" // Can use *
+#define VER_PRODUCTVERSION		1,3,0,0
+#define VER_PRODUCTVERSION_STR	"1.3.0.0"
+#define VER_ASSEMBLYVERSION_STR	"1.3.0.0" // Can use *
 
 #ifdef _DEBUG
 #ifndef WIN32
-#define VER_FILEDESCRIPTION "SonnetWrapper 64-bit (Debug), based on Cbc 2.10.3"
+#define VER_FILEDESCRIPTION "SonnetWrapper 64-bit (Debug), based on Cbc 2.10.5"
 #else
-#define VER_FILEDESCRIPTION "SonnetWrapper 32-bit (Debug), based on Cbc 2.10.3"
+#define VER_FILEDESCRIPTION "SonnetWrapper 32-bit (Debug), based on Cbc 2.10.5"
 #endif
 #else
 #ifndef WIN32
-#define VER_FILEDESCRIPTION "SonnetWrapper 64-bit, based on Cbc 2.10.3"
+#define VER_FILEDESCRIPTION "SonnetWrapper 64-bit, based on Cbc 2.10.5"
 #else
-#define VER_FILEDESCRIPTION "SonnetWrapper 32-bit, based on Cbc 2.10.3"
+#define VER_FILEDESCRIPTION "SonnetWrapper 32-bit, based on Cbc 2.10.5"
 #endif
 #endif
 
-#define VER_COPYRIGHT "Copyright (C) 2011-2019"
+#define VER_COPYRIGHT "Copyright (C) 2011-2020"
 #define VER_TRADEMARK "This code is licensed under the terms of the Eclipse Public License (EPL)"
 #define VER_FILENAME "SonnetWrapper.dll"
 #define VER_FILECOMMENTS "SonnetWrapper is a managed DLL with wrapper classes around existing C++ COIN-OR classes. This version of SonnetWrapper is based on Cbc 2.9.3. See http://sourceforge.net/projects/sonnet-project and http://www.coin-or.org."
