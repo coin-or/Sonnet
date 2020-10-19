21f4d7f85d0820e489133dfb738394d46e6d33d6
Jan-Willem Goossens <jhmgoossens@hotmail.com>
2020-10-19 22:07:32 +0200

    Fix v16 SonnetExamples project for removed Example7

--- examples/Program.cs
+++ examples/Program.cs
@@ -32,9 +32,6 @@ namespace SonnetExamples
 
             Example6b.Example ex6b = new Example6b.Example();
             ex6b.Run();
-
-            Example7.Example ex7 = new Example7.Example();
-            ex7.Run();
         }
     }
 }
